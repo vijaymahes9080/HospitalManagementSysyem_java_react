33bd7bb337fd0b2d5ec6538afc078820af7ad9a9
vijaymahes9080 <vijaymahes9080@gmail.com>
2026-05-26 14:11:14 +0530

    Rename project to HospitalManagementSysyem_java_react

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HospitalManagementSysyem_react/',
+  base: '/HospitalManagementSysyem_java_react/',
 })
