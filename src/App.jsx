c879e13f897b90120ed7f152a2db15e0e6beeb13
vijaymahes9080 <vijaymahes9080@gmail.com>
2026-05-26 14:09:25 +0530

    Fix navigation using HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
@@ -10,7 +10,7 @@ import './index.css';
 
 function App() {
   return (
-    <Router basename="/HospitalManagementSysyem_react">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminLogin />} />
